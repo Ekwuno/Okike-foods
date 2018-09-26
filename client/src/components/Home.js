7222b81764478bc48df50bcfbb1b57c787863f80
Ade-Joshe <femijay9@gmail.com>
2018-09-26 13:56:36 +0100

    refactored teh homepage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,18 +1,45 @@
-import React, { Component } from 'react';
-import { Link, withRouter } from "react-router-dom";
+import React, { Component, Fragment } from 'react';
+// import { Link, withRouter } from "react-router-dom";
 // import Header from './Header';
 import HomeBanner from './HomeBanner';
+import './home.css'
+import HomeBody from './homebody';
 
-class Home extends Component{
-    constructor(){
+
+
+class Home extends Component {
+    constructor() {
         super();
     }
 
-    render(){
-        return(
-            <div>
-                <HomeBanner/>
-            </div>
+    render() {
+        return (
+            <Fragment>
+                <HomeBanner />
+                <center>
+                    <h3><b> How Easy It is</b></h3>
+                    <div className='icon-pan'>
+                        <div className='icon-box'>
+                            <img src={HomeBanner} alt='' />
+                            <p>I am for this one</p>
+                        </div>
+                        <div className='icon-box'>
+                            <img src={HomeBanner} alt='' />
+                            <p>I am for this one</p>
+                        </div>
+                        <div className='icon-box'>
+                            <img src={HomeBanner} alt='' />
+                            <p>I am for this one</p>
+                        </div>
+                    </div>
+                </center>
+                <center><h2> What's available </h2></center>
+                <div className='bookshelf'>
+                    <HomeBody />
+                    <HomeBody />
+                    <HomeBody />
+                </div>
+            </Fragment>
         )
     }
 }

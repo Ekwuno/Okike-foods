f7bf9e92b68bc6cdde57d2e52fa9adee0802f253
Ade-Joshe <femijay9@gmail.com>
2018-09-26 13:07:16 +0100

    testing the homebody component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import Header from './components/Header'
-import {
-    BrowserRouter as Router,
-    Route
-} from 'react-router-dom';
+// import {
+//     BrowserRouter as Router,
+//     Route
+// } from 'react-router-dom';
 import Home from './components/Home.js'
 import './App.css';
 import './container/css/bootstrap.css'
 import HomeBody from './components/homebody';
-import Homepagedata from './components/homepagedata';
 
 class App extends Component {
     render() {
@@ -17,8 +16,11 @@ class App extends Component {
             <div>
                 <Header />
                 <Home />
-                <HomeBody/>
-                <Homepagedata/>
+                <div className='bookshelf'>
+                    <HomeBody />
+                    <HomeBody />
+                    <HomeBody />
+                </div>
             </div >
         );
     }

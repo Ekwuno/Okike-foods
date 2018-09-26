0ace1abfd826a860a662bbee4c856243f1feaf73
Ade-Joshe <femijay9@gmail.com>
2018-09-26 21:26:09 +0100

    added the recipe card and food menus

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,5 +1,41 @@
-import React from 'react'
+import React, { Component, Fragment } from 'react'
+// import Banner from 
+import './menu.css';
+import HomeBody from './homebody'
 
-const Menu = () => {
-    
+
+class Menu extends Component {
+    render() {
+        return (
+            <Fragment>
+                <div className='MenuRecipe'>
+                    {/* image goes here */}
+                    <div className='MenuRecipeImage'>
+
+                    </div>
+                    <div className='MenuRecipeDetail'>
+                        <h3><b>Breakfast Special</b></h3><br /><br />
+                        <p>Isnt she lovely, Haalleluyah? <br />
+                            she says she loves me, Halleluyah<br />
+                            isnt she lovely, Drille la Simi</p>
+                        <button className='btn btn-success btn-md' style={{ float: 'right' }}>Order Now</button>
+                    </div>
+                </div>
+                <div className='bookshelf'>
+
+                    <HomeBody />
+                    <HomeBody />
+                    <HomeBody />
+                    <HomeBody />
+                    <HomeBody />
+                    <HomeBody />
+                    <HomeBody />
+                    <HomeBody />
+                    <HomeBody />
+                </div>
+            </Fragment>
+        )
+    }
 }
+
+export default Menu

e4edd33efd2e397c9cef4bd27ff48550cbebb913
Ade-Joshe <femijay9@gmail.com>
2018-09-26 21:24:49 +0100

    designed the footer

---
+++ client/src/components/footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div style={{ padding: '0 30px' }}>
+            <div style={{ float: 'left' }}>
+                &copy; 2018 Okike Foods. All Rights Reserved
+            </div>
+            <div style={{ float: 'right' }}>
+                <i className='fab fa-facebook'></i> &nbsp;&nbsp;
+                <i className='fab fa-twitter'></i> &nbsp;&nbsp;
+                <i className='fab fa-instagram'></i> &nbsp;&nbsp;
+            </div>
+            <p style={{ textAlign: 'center', marginLeft: -30 }}>
+                +08100000000000 &nbsp;&nbsp;&nbsp; @info@okikefoods.ng
+            </p>
+        </div>
+    )
+}
+
+export default Footer;

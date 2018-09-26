cd98193e8cb1447ba7bdb88bf3745e493c312b23
Ade-Joshe <femijay9@gmail.com>
2018-09-26 13:55:50 +0100

    changed file to route

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,25 @@
-import React, { Component } from 'react';
-// import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import Header from './components/Header'
-// import {
-//     BrowserRouter as Router,
-//     Route
-// } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home.js'
 import './App.css';
 import './container/css/bootstrap.css'
-import HomeBody from './components/homebody';
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <Header />
-                <Home />
-                <div className='bookshelf'>
-                    <HomeBody />
-                    <HomeBody />
-                    <HomeBody />
-                </div>
-            </div >
+            <BrowserRouter>
+                <Switch>
+                    <Fragment>
+                        <Header />
+                        <Route path='/' exact component={Home} />
+                        <Route path='/menu' exact component={} />
+                        <Route path='/fruitChef' exact component={} />
+                        <Route path='/afiaNkwu' exact component={} />
+                        <Route path='/olc' exact component={} />
+                    </Fragment>
+                </Switch>
+            </BrowserRouter>
         );
     }
 }

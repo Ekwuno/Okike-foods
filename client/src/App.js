11caaa6a506a9d5925519bb6a46ac203523f0f2d
Ade-Joshe <femijay9@gmail.com>
2018-09-26 21:30:36 +0100

    added the routes for some pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,9 @@ import Header from './components/Header'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home.js'
 import './App.css';
-import './container/css/bootstrap.css'
+import './container/css/bootstrap.css';
+import Footer from './components/footer'
+import Menu from './components/Menu'
 
 class App extends Component {
     render() {
@@ -13,10 +15,11 @@ class App extends Component {
                     <Fragment>
                         <Header />
                         <Route path='/' exact component={Home} />
-                        <Route path='/menu' exact component={} />
-                        <Route path='/fruitChef' exact component={} />
+                        <Route path='/menu' exact component={Menu} />
+                        {/* <Route path='/fruitChef' exact component={} />
                         <Route path='/afiaNkwu' exact component={} />
-                        <Route path='/olc' exact component={} />
+                        <Route path='/olc' exact component={} /> */}
+                        <Footer />
                     </Fragment>
                 </Switch>
             </BrowserRouter>

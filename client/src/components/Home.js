d39bcf78b4a795ea851498b105b6295c2a4cfda6
Ade-Joshe <femijay9@gmail.com>
2018-09-26 21:24:04 +0100

    added pictures for the icons on the homepage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,8 +4,9 @@ import React, { Component, Fragment } from 'react';
 import HomeBanner from './HomeBanner';
 import './home.css'
 import HomeBody from './homebody';
-
-
+import serving from '../assets/serving-dish.svg';
+import group from '../assets/Group.svg';
+import call from '../assets/menu (2).svg'
 
 class Home extends Component {
     constructor() {
@@ -20,25 +21,45 @@ class Home extends Component {
                     <h3><b> How Easy It is</b></h3>
                     <div className='icon-pan'>
                         <div className='icon-box'>
-                            <img src={HomeBanner} alt='' />
+                            <img src={call} alt='' />
                             <p>I am for this one</p>
                         </div>
                         <div className='icon-box'>
-                            <img src={HomeBanner} alt='' />
+                            <img src={group} alt='' />
                             <p>I am for this one</p>
                         </div>
                         <div className='icon-box'>
-                            <img src={HomeBanner} alt='' />
+                            <img src={serving} alt='' />
                             <p>I am for this one</p>
                         </div>
                     </div>
                 </center>
-                <center><h2> What's available </h2></center>
+                <center>
+                    <h2> What's available </h2>
+                    </center>
                 <div className='bookshelf'>
                     <HomeBody />
                     <HomeBody />
                     <HomeBody />
                 </div>
+                <center>
+                    <h3><b>Our Offerings</b></h3>
+                    <div className='icon-pan'>
+                        <div className='icon-box'>
+                            <img src={HomeBanner} alt='' />
+                            <p>Afia Nkwu</p>
+                        </div>
+                        <div className='icon-box'>
+                            <img src={HomeBanner} alt='' />
+                            <p>Fruit Chef</p>
+                        </div>
+                        <div className='icon-box'>
+                            <img src={HomeBanner} alt='' />
+                            <p>OLC</p>
+                        </div>
+                    </div>
+                </center>
+
             </Fragment>
         )
     }

ccac048dbdcbba80e0356aadbf6d260d6ccd238a
Ade-Joshe <femijay9@gmail.com>
2018-09-28 19:54:48 +0100

    allowed order accept propos to change page text

--- client/src/components/Order.js
+++ client/src/components/Order.js
@@ -1,21 +1,55 @@
-import React, { Component } from 'react';
-import './fruitChef.css'
+import React, { Component, Fragment } from 'react';
+import Footer from './footer'
 
 class Order extends Component {
-    render() {
-        return (
-            <div className='specialCard'>
-                <div className='MenuRecipeImage'>
 
+    state = {
+        title: null,
+        description: null,
+        orderText: null
+    }
+
+    componentDidMount() {
+        const id = this.props.match.params.id;
+        (id === 'olc') ?
+            this.setState({
+                title: 'Owambe Gigi Gaan',
+                description: 'Awon boys ti yapa si adugbo',
+                orderText: 'i want to order for delicacies'
+            }) :
+            (id === 'afiaNkwu') ?
+                this.setState({
+                    title: 'Awon AfiaNkwu Ni Adugbo',
+                    description: 'Afia Nkwu issa goal, issa bae sontin',
+                    orderText: 'I want to order for palm-wine( Nkwu )'
+                })
+                :
+                (id === 'fruitChef') ?
+                    this.setState({
+                        title: 'Fruit Chef something',
+                        description: 'This fruitty drink is so making sense',
+                        orderText: 'i want to order for fruit drinks/smoothies'
+                    })
+                    : null
+    }
+
+    render() {
+        const { title, description } = this.state;
+        const orderText = encodeURI(this.state.orderText);
+        return (
+            <Fragment>
+                <div className='specialCard'>
+                    <div className='MenuRecipeImage'>
+
+                    </div>
+                    <div className='MenuRecipeDetail'>
+                        <h3><b>{title}</b></h3><br /><br />
+                        <p>{description}</p>
+                        <button onClick={() => { window.open(`https://wa.me/15551234567?text=${orderText}`) }} className='btn btn-success btn-md' style={{ float: 'right' }}>Order Now</button>
+                    </div>
                 </div>
-                <div className='MenuRecipeDetail'>
-                    <h3><b>Breakfast Special</b></h3><br /><br />
-                    <p>Isnt she lovely, Haalleluyah? <br />
-                        she says she loves me, Halleluyah<br />
-                        isnt she lovely, Drille la Simi</p>
-                    <button className='btn btn-success btn-md' style={{ float: 'right' }}>Order Now</button>
-                </div>
-            </div>
+                <Footer position='absolute' />
+            </Fragment>
         )
     }
 }

c52213214ba3ad29939526736862c3f9fb90bcf7
Ade-Joshe <femijay9@gmail.com>
2018-09-28 19:57:20 +0100

    set state for dirrerent components

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,21 +1,41 @@
-import React from 'react'
+import React, { Fragment, Component } from 'react'
 
-const Footer = () => {
-    return (
-        <div className='footerStyle'>
-            <div style={{ float: 'left' }}>
-                &copy; 2018 Okike Foods. All Rights Reserved
+class Footer extends Component {
+    state = {
+        positionval: null,
+    }
+
+    componentDidMount() {
+        (this.props.position === undefined) ?
+            this.setState({
+                positionval: 'unset'
+            }) :
+            this.setState({
+                positionval: 'absolute'
+            });
+    }
+
+    render() {
+        const { positionval } = this.state
+        console.log(positionval);
+        return (
+            <Fragment>
+                <div className='footerStyle' style={{ position: `${positionval}` }}>
+                    <div style={{ float: 'left' }}>
+                        &copy; 2018 Okike Foods. All Rights Reserved
             </div>
-            <div style={{ float: 'right' }}>
-                <i className='fab fa-facebook'></i> &nbsp;&nbsp;
+                    <div style={{ float: 'right' }}>
+                        <i className='fab fa-facebook'></i> &nbsp;&nbsp;
                 <i className='fab fa-twitter'></i> &nbsp;&nbsp;
                 <i className='fab fa-instagram'></i> &nbsp;&nbsp;
             </div>
-            <p style={{ textAlign: 'center', marginLeft: -30 }}>
-                +08100000000000 &nbsp;&nbsp;&nbsp; @info@okikefoods.ng
+                    <p style={{ textAlign: 'center', marginLeft: -30 }}>
+                        +08100000000000 &nbsp;&nbsp;&nbsp; @info@okikefoods.ng
             </p>
-        </div>
-    )
+                </div>
+            </Fragment>
+        )
+    }
 }
 
 export default Footer;

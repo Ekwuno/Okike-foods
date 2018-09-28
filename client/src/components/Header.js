1b538ddbeea2174927d2ba232beff9b7c120b874
Ade-Joshe <femijay9@gmail.com>
2018-09-28 19:57:06 +0100

    added the whatsapp links to the buttons and fixed routing

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Link, withRouter } from "react-router-dom";
+import React from 'react';
+import { NavLink } from "react-router-dom";
 import Green from '../assets/green.svg';
 import HeaderLogo from '../assets/logo.svg';
 import OkikeWriteUp from '../assets/okike_foods.svg';
@@ -7,16 +7,19 @@ import './Header.css';
 
 const Header = () => {
     return (
-        <div className="custom-header" style={{top: 0}}>
+        <div className="custom-header" style={{ top: 0 }}>
             <div className="header-logo">
-                <img src={HeaderLogo}/>   
-                <img className="header-writeup" src={OkikeWriteUp}/>              
+                <NavLink to='/'>
+                    <img src={HeaderLogo} />
+                    <img className="header-writeup" src={OkikeWriteUp} />
+                </NavLink>
             </div>
-            <div className="green"> <img src={Green}/>
-                <button>Whatsapp Okike</button>
+            <div className="green"> <img src={Green} />
+                <button  onClick={() => { window.open('https://wa.me/15551234567?text') }}>Whatsapp Okike</button>
             </div>
         </div>
-         )
+    )
 };
+
 
 export default Header;
